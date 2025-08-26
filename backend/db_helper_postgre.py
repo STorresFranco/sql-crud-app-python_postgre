b1483251fc1aeea30602e7e9f474e44e4f7bf178
STorresFranco <136782032+STorresFranco@users.noreply.github.com>
2025-08-26 00:01:37 -0500

    Update db_helper_postgre.py

--- backend/db_helper_postgre.py
+++ backend/db_helper_postgre.py
@@ -294,10 +294,7 @@ def expense_summary(start_date,end_date):
         try:
             cursor.execute(query,params)
             total_expenses=cursor.fetchall()
-            if len(total_expenses)>0:
-                logger.info("Date range retrieved successfuly for analytics")
-            else:
-                raise RuntimeError("Error retrieving date range")
+            logger.info("Date range retrieved successfuly for analytics")
         except Exception as e:
             logger.error("Failed to retrieve date range for analytics")
             raise RuntimeError("Error retrieving date range")
@@ -310,20 +307,19 @@ def expense_summary(start_date,end_date):
     params=list([start_date,end_date])
 
     with get_db_cursor() as cursor:
-    try:
-        cursor.execute(query, params)
-        top_expenses = cursor.fetchall()
-        if len(top_expenses) > 0:
+        try:
+            cursor.execute(query, params)
+            top_expenses = cursor.fetchall()
+            if len(top_expenses) == 0 and len(total_expenses) == 0:
+                logger.warning(f"No expenses found for range {start_date} to {end_date}")
+                raise RuntimeError("No data available for the selected date range")
             logger.info(f"Retrieved {len(top_expenses)} top expenses for range {start_date} to {end_date}")
-        else:
-            logger.warning(f"No expenses found for range {start_date} to {end_date}")
-            raise RuntimeError("No data available for the selected date range")
-
-    except Exception as e:
-        logger.error(f"Failed to retrieve top expenses: {e}")
-        raise RuntimeError("Error retrieving top expenses")
+        except Exception as e:
+            logger.error(f"Failed to retrieve top expenses: {e}")
+            raise RuntimeError("Error retrieving top expenses")
 
     return total_expenses,top_expenses
+
 
 
 
